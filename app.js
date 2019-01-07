ee24ba4206817bdb7e39b1a970459f2b6f4da259
zhaobin <703780521@qq.com>
2019-01-07 17:25:50 +0800

    init

---
+++ app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var infosRouter = require('./routes/infos');
+var cinemasRouter = require('./routes/cinemas');
+var filmsRouter = require('./routes/films');
+var filmAndCinemasRouter = require('./routes/filmAndCinemas');
+var soonsRouter = require('./routes/soons');
+var hotsRouter = require('./routes/hots');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(session({
+  secret: "lovo",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 60 }
+}))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/index', indexRouter);
+app.use('/users', usersRouter);
+app.use('/infos', infosRouter);
+app.use('/cinemas', cinemasRouter);
+app.use('/films', filmsRouter);
+app.use('/filmAndCinemas', filmAndCinemasRouter);
+app.use('/soons', soonsRouter);
+app.use('/hots', hotsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.log(err);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
